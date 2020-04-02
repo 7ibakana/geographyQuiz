b6a3b490b62f9a2c17b569b87ff5857638a9a6ab
yu2519hn <1234568hH*>
2020-04-02 09:24:47 -0500

    comments added

--- Form1.cs
+++ Form1.cs
@@ -20,14 +20,14 @@ namespace geographyQuiz
 
         private void btnCheckAnswer_Click(object sender, EventArgs e)
         {
-            string answer = txtAnswer.Text;
-            answer = answer.ToLower();
-            if (answer.Contains("pacific"))
-            {
+            string answer = txtAnswer.Text; //The txtAnswer text entered is a string called answer
+            answer = answer.ToLower(); //The answer is lowercase
+            if (answer.Contains("pacific")) //if the answer contains "pacific"
+            {//The messagebox will show Correct for the result
                 MessageBox.Show("Correct!", "Result");
             }
-            else
-            {
+            else //Otherwise
+            {//The messagebox will show that its incorrect and present the correct answer for the result
                 MessageBox.Show("Incorrect, the answer is the Pacific Ocean", "Result");
             }
         }
